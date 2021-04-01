88cafd69091cc7e9023fc213f59218b2c14bbe97
ilya kamliuk <douseethem@gmail.com>
2021-04-01 19:28:17 +0200

    git init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useAuth0 } from "@auth0/auth0-react";
+import ProtectedRoute from "./auth/protected-route";
+
+import Navbar from "./components/Navbar"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Loading from './components/Loading';
+import ButtonVar from './components/Button';
+import InputForm from "./components/InputForm";
+import Profile from "./components/Profile";
+import {UserContext} from './components/UserContext';
+
+
+import React,{ useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
+import TaskCreate from "./components/TaskCreate";
+import TaskList from "./components/TaskList";
+
+export default function App() {
+  
+ 
+  const { user, isLoading } = useAuth0();
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <>
+    <Navbar/ >
+     <Switch>
+     <UserContext.Provider value = {user}>
+     <Route path="/" exact component={isLoading?Loading:Profile}  />
+          <ProtectedRoute path="/profile" component={Profile} />
+          <ProtectedRoute path="/newtask" component={TaskCreate} />
+          <ProtectedRoute path="/tasks" component={TaskList} />
+          </UserContext.Provider>
+     </Switch>
+     </>
+    </Router>
   );
 }
 
-export default App;
+
+
